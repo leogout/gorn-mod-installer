505d47ce839f7d830f0a078525661da5326225ac
RICHARD Leo <leo.richard@edf.fr>
2018-12-19 16:41:28 +0100

    Added options from enum and map to combo box

--- src/view/PlatformSelection.cpp
+++ src/view/PlatformSelection.cpp
@@ -13,5 +13,12 @@ PlatformSelection::PlatformSelection() {
 
     config_layout->addWidget(m_platform_input);
 
+    QMap<QString, GamePlatform> platform_options;
+
+    platform_options.insert("Oculus", GamePlatform::Oculus);
+    platform_options.insert("Steam", GamePlatform::Steam);
+
+    m_platform_input->addItems(QStringList(platform_options.keys()));
+
     setLayout(config_layout);
 }
